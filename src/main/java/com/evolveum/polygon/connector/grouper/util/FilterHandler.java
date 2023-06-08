0c59cc07973cad8380faa4732365327edc6ec628
mmacik <matus.macik@evolveum.com>
2023-06-08 09:28:43 +0200

    Copyright additions

--- src/main/java/com/evolveum/polygon/connector/grouper/util/FilterHandler.java
+++ src/main/java/com/evolveum/polygon/connector/grouper/util/FilterHandler.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.evolveum.polygon.connector.grouper.util;
 
 import org.identityconnectors.common.logging.Log;
